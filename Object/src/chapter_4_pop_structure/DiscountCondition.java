7117a9554bc03328c159ec4c7af7842369c390d6
Chocobe <nice_10bird@naver.com>
2019-08-27 13:57:08 +0900

    <master> : Fixed DiscountCondition

--- Object/src/chapter_4_pop_structure/DiscountCondition.java
+++ Object/src/chapter_4_pop_structure/DiscountCondition.java
@@ -12,61 +12,93 @@ public class DiscountCondition {
 	private LocalTime startTime;
 	private LocalTime endTime;
 	
-	
-// type
-	// accessor
+
+// 할인 조건
 	public DiscountConditionType getType() {
 		return type;
 	}
 	
-	// mutator
-	public void setType(DiscountConditionType type) {
-		this.type = type;
+	
+// 할인 조건에 맞는가?
+	// 기간조건 Overloading
+	public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+		if(type != DiscountConditionType.PERIOD) {
+			throw new IllegalArgumentException();
+			
+		}
+		
+		return this.dayOfWeek.equals(dayOfWeek) &&
+			   startTime.compareTo(time) <= 0 &&
+			   endTime.compareTo(time) >= 0;
 	}
 	
 	
-// sequence
-	// accessor
-	public int getSequence() {
-		return sequence;
-	}
-	
-	// mutator
-	public void setSequence(int sequence) {
-		this.sequence = sequence;
-	}
-	
-	
-// dayOfWeek
-	public DayOfWeek getDayOfWeek() {
-		return dayOfWeek;
-	}
-	
-	public void setDayOfWeek(DayOfWeek dayOfWeek) {
-		this.dayOfWeek = dayOfWeek;
+	// 순번조건 Overloading
+	public boolean isDisCountable(int sequence) {
+		if(type != DiscountConditionType.SEQUENCE) {
+			throw new IllegalArgumentException();
+		}
+		
+		return this.sequence == sequence;
 	}
 	
 	
-// startTime
-	// accessor
-	public LocalTime getStartTime() {
-		return startTime;
-	}
+// 캡슐화를 위한 변경
 	
-	// mutator
-	public void setStartTime(LocalTime startTime) {
-		this.startTime = startTime;
-	}
-	
-	
-// endTime
-	// accessor
-	public LocalTime getEndTime() {
-		return endTime;
-	}
-	
-	// mutator
-	public void setEndTime(LocalTime endTime) {
-		this.endTime = endTime;
-	}
+//// type
+//	// accessor
+//	public DiscountConditionType getType() {
+//		return type;
+//	}
+//	
+//	// mutator
+//	public void setType(DiscountConditionType type) {
+//		this.type = type;
+//	}
+//	
+//	
+//// sequence
+//	// accessor
+//	public int getSequence() {
+//		return sequence;
+//	}
+//	
+//	// mutator
+//	public void setSequence(int sequence) {
+//		this.sequence = sequence;
+//	}
+//	
+//	
+//// dayOfWeek
+//	public DayOfWeek getDayOfWeek() {
+//		return dayOfWeek;
+//	}
+//	
+//	public void setDayOfWeek(DayOfWeek dayOfWeek) {
+//		this.dayOfWeek = dayOfWeek;
+//	}
+//	
+//	
+//// startTime
+//	// accessor
+//	public LocalTime getStartTime() {
+//		return startTime;
+//	}
+//	
+//	// mutator
+//	public void setStartTime(LocalTime startTime) {
+//		this.startTime = startTime;
+//	}
+//	
+//	
+//// endTime
+//	// accessor
+//	public LocalTime getEndTime() {
+//		return endTime;
+//	}
+//	
+//	// mutator
+//	public void setEndTime(LocalTime endTime) {
+//		this.endTime = endTime;
+//	}
 }
