1ce84f2993e76d58468bda371b9d7bd147078ceb
Chocobe <nice_10bird@naver.com>
2019-08-29 09:29:12 +0900

    <master> : Created SequenceCondition

---
+++ Object/src/chapter_5_responsibility/SequenceCondition.java
@@ -0,0 +1,15 @@
+package chapter_5_responsibility;
+
+public class SequenceCondition {
+	private int sequence;
+	
+	
+	public SequenceCondition(int sequence) {
+		this.sequence = sequence;
+	}
+	
+	
+	public boolean isSatisfiedBy(Screening screening) {
+		return sequence == screening.getSequence();
+	}
+}
