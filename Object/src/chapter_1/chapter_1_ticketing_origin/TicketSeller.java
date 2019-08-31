aaf251fb237abdec3b63ce27967ade97f73e52e2
Chocobe <nice_10bird@naver.com>
2019-08-31 16:16:32 +0900

    <master> : Created class TicketSeller

---
+++ Object/src/chapter_1/chapter_1_ticketing_origin/TicketSeller.java
@@ -0,0 +1,15 @@
+package chapter_1.chapter_1_ticketing_origin;
+
+public class TicketSeller {
+	private TicketOffice ticketOffice;
+	
+	
+	public TicketSeller(TicketOffice ticketOffice) {
+		this.ticketOffice = ticketOffice;
+	}
+	
+	
+	public TicketOffice getTicketOffice() {
+		return ticketOffice;
+	}
+}
