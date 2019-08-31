cc8429d39c5d248c3d143d87fed514961d54bfeb
Chocobe <nice_10bird@naver.com>
2019-08-31 16:22:18 +0900

    <master> : Created class Theater

---
+++ Object/src/chapter_1/chapter_1_ticketing_origin/Theater.java
@@ -0,0 +1,26 @@
+package chapter_1.chapter_1_ticketing_origin;
+
+public class Theater {
+	private TicketSeller ticketSeller;
+	
+	
+	public Theater(TicketSeller ticketSeller) {
+		this.ticketSeller = ticketSeller;
+	}
+	
+	
+	public void enter(Audience audience) {
+		if(audience.getBag().hasInvitation()) {
+			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+			audience.getBag().setTicket(ticket);
+			
+		} else {
+			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+			ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+			
+			audience.getBag().minusAmount(ticket.getFee());
+			audience.getBag().setTicket(ticket);
+			
+		}
+	}
+}
