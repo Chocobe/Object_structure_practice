d05a37dd5f9e4821adb8c3884151b0f8cf60894c
Chocobe <nice_10bird@naver.com>
2019-09-09 21:56:19 +0900

    <master> : Refactored InstrumentedHashSet

---
+++ Object/src/chapter_11_composition/chapter_11_01_inheritance_problem/InstrumentedHashSet.java
@@ -0,0 +1,32 @@
+package chapter_11_composition.chapter_11_01_inheritance_problem;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class InstrumentedHashSet<E> {
+	private int addCount = 0;
+	private Set<E> set;
+	
+	
+	
+	public InstrumentedHashSet(Set<E> set) {
+		this.set = set;
+	}
+	
+	
+	public boolean add(E e) {
+		addCount++;
+		return set.add(e);
+	}
+	
+	
+	public boolean addAll(Collection<? extends E> c) {
+		addCount += c.size();
+		return set.addAll(c);
+	}
+	
+	
+	public int getAddCount() {
+		return addCount;
+	}
+}
