4a1c8e075162a7cc838aa4ed21a6188eadc6d85c
Chocobe <nice_10bird@naver.com>
2019-09-09 22:12:24 +0900

    <master> : Refactored class InstrumentedHashSet

--- Object/src/chapter_11_composition/chapter_11_01_inheritance_problem/InstrumentedHashSet.java
+++ Object/src/chapter_11_composition/chapter_11_01_inheritance_problem/InstrumentedHashSet.java
@@ -1,13 +1,14 @@
 package chapter_11_composition.chapter_11_01_inheritance_problem;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.Spliterator;
 
-public class InstrumentedHashSet<E> {
+public class InstrumentedHashSet<E> implements Set<E> {
 	private int addCount = 0;
 	private Set<E> set;
-	
-	
+
 	
 	public InstrumentedHashSet(Set<E> set) {
 		this.set = set;
@@ -28,5 +29,89 @@ public class InstrumentedHashSet<E> {
 	
 	public int getAddCount() {
 		return addCount;
+	}
+
+
+	@Override
+	public boolean remove(Object o) {
+		return set.remove(o);
+	}
+	
+	
+	@Override
+	public void clear() {
+		set.clear();		
+	}	
+	
+	
+	@Override
+	public boolean equals(Object o) {
+		return set.equals(o);
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return set.hashCode();
+	}
+
+	
+	@Override
+	public Spliterator<E> spliterator() {
+		return set.spliterator();
+	}
+	
+
+	@Override
+	public int size() {
+		return set.size();
+	}
+
+	
+	@Override
+	public boolean isEmpty() {
+		return set.isEmpty();
+	}
+
+	
+	@Override
+	public boolean contains(Object o) {
+		return set.contains(o);
+	}
+
+	
+	@Override
+	public Iterator<E> iterator() {
+		return set.iterator();
+	}
+
+	
+	@Override
+	public Object[] toArray() {
+		return set.toArray();
+	}
+
+	
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return set.toArray(a);
+	}
+	
+	
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return set.containsAll(c);
+	}
+	
+	
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return set.retainAll(c);
+	}
+
+	
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return set.removeAll(c);
 	}
 }
