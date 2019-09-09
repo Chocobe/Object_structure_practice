1279c1d85912a64f968a900819f25923b799c2e9
Chocobe <nice_10bird@naver.com>
2019-09-09 22:52:22 +0900

    <master> : prepared Refactoring

--- Object/src/chapter_11_composition/chapter_11_02_phone_combine_policy/NightlyDiscountPhone.java
+++ Object/src/chapter_11_composition/chapter_11_02_phone_combine_policy/NightlyDiscountPhone.java
@@ -9,14 +9,10 @@ public class NightlyDiscountPhone extends Phone {
 	private Money regularAmount;
 	private Duration seconds;
 
-	// 추상 클래스로 이동
-//	private List<Call> calls = new ArrayList<Call>();
-	
 	
 // 생성자
 	public NightlyDiscountPhone(Money nightlyAmount, 
-					Money regularAmount, Duration seconds, double taxRate) {
-		super(taxRate);
+					Money regularAmount, Duration seconds) {
 		this.nightlyAmount = nightlyAmount;
 		this.regularAmount = regularAmount;
 		this.seconds = seconds;
@@ -33,17 +29,4 @@ public class NightlyDiscountPhone extends Phone {
 			return regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
 		}
 	}
-	
-
-	// 추상 클래스로 이동
-//// 요금 계산하기
-//	public Money calculateFee() {
-//		Money result = Money.ZERO;
-//		
-//		for(Call call : calls) {
-//			result = result.plus(calculateCallFee(call));
-//		}
-//		
-//		return result;
-//	}
 }
