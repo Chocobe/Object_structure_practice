b4fcf792d1b9bdb768b1b7a454107d1ef2a76eaa
Chocobe <nice_10bird@naver.com>
2019-09-15 20:17:34 +0900

    <master> : Created StreamTest_9

---
+++ Object/src/practice/stream/StreamTest_8.java
@@ -0,0 +1,18 @@
+package practice.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest_8 {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello", "World", "Java");
+		
+		list.stream().map(x -> x + "!").forEach(System.out::println);
+		System.out.println();
+		
+		list.stream().mapToInt(String::length).forEach(System.out::println);
+		System.out.println();
+		
+		System.out.println(list.stream().mapToInt(String::length).sum());
+	}
+}
---
+++ Object/src/practice/stream/StreamTest_9.java
@@ -0,0 +1,24 @@
+package practice.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest_9 {
+	public static void main(String[] args) {
+		List<String> listStr = Arrays.asList("Hello", "Java", "Hi", "World");
+		
+		int len = listStr.stream().mapToInt(String::length).sum();
+		System.out.println(len);
+		
+		List<Integer> listInt = Arrays.asList(1, 2, 3, 4, 5);
+		System.out.println();
+		
+		int sum = listInt.stream().mapToInt(Integer::intValue).sum();
+		int avg = (int)listInt.stream().mapToInt(Integer::intValue).average().orElse(0);
+		int max = listInt.stream().mapToInt(Integer::intValue).max().orElse(0);
+		int min = listInt.stream().mapToInt(Integer::intValue).min().orElse(0);
+		
+		List<Integer> resultList = Arrays.asList(sum, avg, max, min);
+		resultList.stream().forEach(System.out::println);
+	}
+}
