c80f1dc0c67e4814936584e1dca70fce7a02e421
Chocobe <nice_10bird@naver.com>
2019-09-16 21:19:52 +0900

    <master> : Created class Grade

--- Object/src/chapter_12_polymorphism/chapter_12_school_score/Grade.java
+++ Object/src/chapter_12_polymorphism/chapter_12_school_score/Grade.java
@@ -1,5 +1,30 @@
 package chapter_12_polymorphism.chapter_12_school_score;
 
 public class Grade {
-
+	private String name;
+	private int upper;
+	private int lower;
+	
+	
+// 생성자
+	public Grade(String name, int upper, int lower) {
+		this.name = name;
+		this.upper = upper;
+		this.lower = lower;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public boolean isName(String name) {
+		return this.name.equals(name);
+	}
+	
+	
+	public boolean include(int score) {
+		return score >= lower && score <= upper;
+	}
 }
