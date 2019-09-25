9c9499ce48216f23395abcc824692409a51367cb
Chocobe <nice_10bird@naver.com>
2019-09-25 19:20:50 +0900

    <master> : Fixed class Call

--- Object/src/chapter_14_phone/chapter_14_01_phone_origin/Call.java
+++ Object/src/chapter_14_phone/chapter_14_01_phone_origin/Call.java
@@ -4,19 +4,30 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Call {
-	private LocalDateTime from;
-	private LocalDateTime to;
+	DateTimeInterval interval;
+	
 	
 	public Call(LocalDateTime from, LocalDateTime to) {
-		this.from = from;
-		this.to = to;
+		this.interval = DateTimeInterval.of(from, to);
 	}
 	
-	public Duration getSeconds() {
-		return Duration.between(from, to);
+	
+	public Duration getDuration() {
+		return interval.duration();
 	}
+	
 	
 	public LocalDateTime getFrom() {
-		return from;
+		return interval.getFrom();
+	}
+	
+	
+	public LocalDateTime getTo() {
+		return interval.getTo();
+	}
+	
+	
+	public DateTimeInterval getInterval() {
+		return interval;
 	}
 }
